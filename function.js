317aa24debfe1ae66a458c20eabb4664e29b4ed7
shahidulgod <shahidulgod@gmail.com>
2023-08-02 10:27:39 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,21 @@
+// function declaration 
+function startFan(){
+    console.log('stand up')
+    console.log('walk towards the switch')
+    console.log('press the switch')
+}
+    
+
+// call the function
+startFan();
+console.log('mummi basai')
+console.log('good morning')
+startFan();
+
+// function parameter
+function bringsBanana(money){
+    console.log('mama banana den')
+    console.log(money)
+
+}
+bringsBanana(100)
